9bd47877afcbb6f23c2943dca78a7f078385dcd1
张然 <zr41157@ly.com>
2016-10-17 17:07:17 +0800

    study

---
+++ tools/bom.js
@@ -0,0 +1,25 @@
+/**
+ * Created by oospace on 2016/10/17.
+ * browser
+ */
+//window
+
+//location
+
+//navigator
+
+//screen
+
+//history
+
+//cookie
+
+//session
+
+//storage
+
+//indexDB
+
+
+
+//Attribute
---
+++ tools/dom.js
@@ -0,0 +1,10 @@
+/**
+ * Created by oospace on 2016/10/17.
+ * html
+ */
+
+//document
+
+//Event
+
+//iframe
